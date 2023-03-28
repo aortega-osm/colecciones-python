79c6422ec0b7ee4141edb7d1484761d64f12c87f
Alejandro ortega <aortega@osm-soft.io>
2023-03-28 17:21:28 -0300

    Realice 2 ejercicios Ejercicio 2 realiza una lista de los numeros menores a 5 Ejercicio1 hice un rango del 1al10 y luego imprimi los divisores de 3

---
+++ Ejercicio.py
@@ -0,0 +1,17 @@
+#Ejercicio 1
+for n in range(11):
+    if n % 3 == 0:
+        print(n)
+#Ejercicio 2
+print("Rango del 2 al 6")
+Rango=range(2,7)
+for n in Rango:
+    print(n)
+#Ejercicio 3
+print("Rango con incremento")
+rango=range(3,11,2)
+for n in rango:
+    print(n)
+    
+    
+    
